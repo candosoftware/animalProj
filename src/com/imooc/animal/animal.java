6de7c6f1e08cfa01dccda15c6e2b0439d45d6089
terry <terry@192.168.0.107>
2018-06-12 22:54:12 +0800

    第一次修改

---
+++ src/com/imooc/animal/animal.java
@@ -0,0 +1,13 @@
+package com.imooc.animal;
+
+public class animal {
+       private static String name;
+       
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+    System.out.println("test");
+    System.out.println(name);
+	}
+
+}
